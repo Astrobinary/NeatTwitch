e62c55c4d11f3bdfa4a21a18ce2e3471dbc311ed
Brandon <brandilla1337@gmail.com>
2018-11-14 03:40:27 -0500

    Updated store

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import "./global.css";
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import AppReducer from "./reducers";
 
@@ -17,9 +17,7 @@ import Footer from "./components/Footer/Footer";
 import _Game from "./components/GameSection/gamesection";
 import _Streamer from "./components/StreamerSection/streamersection";
 
-const composeEnhancer = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose;
-
-const store = createStore(AppReducer, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(AppReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
 
 const App = () => (
 	<Router>

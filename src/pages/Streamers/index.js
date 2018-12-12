4114ef033eb7e7b67c57e0756de9aaffcf1d0724
Brandon <brandilla1337@gmail.com>
2018-12-12 14:13:40 -0500

    Visually normalized sorting

--- src/pages/Streamers/index.js
+++ src/pages/Streamers/index.js
@@ -8,96 +8,92 @@ import "./streamers.scss";
 import optionIcon from "./options.svg";
 
 class Streamers extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			showMenu: false,
-			menuItems: ["youtube", "followed"],
-			currntMenu: "twitch"
-		};
-	}
+    this.state = {
+      showMenu: false,
+      menuItems: ["youtube", "followed"],
+      currntMenu: "twitch"
+    };
+  }
 
-	componentDidMount() {
-		if (this.props.twitch.length === 0) this.props.fetch();
-	}
+  componentDidMount() {
+    if (this.props.twitch.length === 0) this.props.fetch();
+  }
 
-	getMoreStreamers = () => {
-		this.props.fetchAgain(this.props.offset + 101);
-	};
+  getMoreStreamers = () => {
+    this.props.fetchAgain(this.props.offset + 101);
+  };
 
-	toggleMenu = () => {
-		this.setState({ showMenu: !this.state.showMenu });
-	};
+  toggleMenu = () => {
+    this.setState({ showMenu: !this.state.showMenu });
+  };
 
-	updateMenu = newItem => {
-		let temp = this.state.menuItems.filter(i => i !== newItem);
-		temp.push(this.state.currntMenu);
+  updateMenu = newItem => {
+    let temp = this.state.menuItems.filter(i => i !== newItem);
+    temp.push(this.state.currntMenu);
 
-		this.setState({ currntMenu: newItem, menuItems: temp, showMenu: !this.state.showMenu });
-	};
+    this.setState({ currntMenu: newItem, menuItems: temp, showMenu: !this.state.showMenu });
+  };
 
-	render() {
-		const streamerItems = this.props.twitch.map(x => (
-			<Link className="streamer-item" key={uid(x)} to={`${this.props.match.url}/${x.channel.name}`}>
-				<img src={x.channel.logo.replace("300x300", "150x150")} alt={x.channel.name} />
-				<div className="streamer-name">{x.channel.name}</div>
-			</Link>
-		));
+  render() {
+    const streamerItems = this.props.twitch.map(x => (
+      <Link className="streamer-item" key={uid(x)} to={`${this.props.match.url}/${x.channel.name}`}>
+        <img src={x.channel.logo.replace("300x300", "150x150")} alt={x.channel.name} />
+        <div className="streamer-name">{x.channel.name}</div>
+      </Link>
+    ));
 
-		const loadGif = (
-			<div>
-				<img src={loadIcon} alt="load icon" />
-			</div>
-		);
+    const loadGif = (
+      <div>
+        <img src={loadIcon} alt="load icon" />
+      </div>
+    );
 
-		const menu = (
-			<div className="menu">
-				{this.state.menuItems.map(item => {
-					return (
-						<span onClick={() => this.updateMenu(item)} key={uid(item)}>
-							{item}
-						</span>
-					);
-				})}
-			</div>
-		);
+    return (
+      <section className="Streamers">
+        <div className="streamers-options">
+          <img src={optionIcon} alt="options" />
+          <span>SORT BY</span>
 
-		return (
-			<section className="Streamers">
-				<div className="streamers-options">
-					<img src={optionIcon} alt="options" />
-					<span>SORT BY</span>
-					{this.state.showMenu ? menu : null}
-					<span className="options-choice" onClick={this.toggleMenu}>
-						{this.state.currntMenu}
-					</span>
-					<span>STREAMERS</span>
-				</div>
+          <span className="options-choice" onClick={this.toggleMenu}>
+            {this.state.currntMenu}
+          </span>
+          {this.state.showMenu ? (
+            <div className="streamers-menu">
+              <div onClick={() => this.updateMenu("followed")}>Followed</div>
+              <div onClick={() => this.updateMenu("youtube")}>YouTube</div>
+              <div onClick={() => this.updateMenu("twitch")}>Twitch</div>
+              <div onClick={() => this.updateMenu("all")}>All</div>
+            </div>
+          ) : null}
+          <span>STREAMERS</span>
+        </div>
 
-				<section className="streamer-container">{this.props.loading ? loadGif : streamerItems}</section>
-			</section>
-		);
-	}
+        <section className="streamer-container">{this.props.loading ? loadGif : streamerItems}</section>
+      </section>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	return {
-		twitch: state.streamersReducer._twitch,
-		loading: state.streamersReducer.loading,
-		error: state.streamersReducer.error,
-		offset: state.streamersReducer.offset
-	};
+  return {
+    twitch: state.streamersReducer._twitch,
+    loading: state.streamersReducer.loading,
+    error: state.streamersReducer.error,
+    offset: state.streamersReducer.offset
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-	return {
-		fetch: () => dispatch(fetchStreamers()),
-		fetchAgain: offset => dispatch(fetchMoreStreamers(offset))
-	};
+  return {
+    fetch: () => dispatch(fetchStreamers()),
+    fetchAgain: offset => dispatch(fetchMoreStreamers(offset))
+  };
 };
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Streamers);

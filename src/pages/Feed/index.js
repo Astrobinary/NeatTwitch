0b6d4c3cb9faed32466c7c2d2261df5c37028459
Brandon <brandilla1337@gmail.com>
2018-12-26 14:39:19 -0500

    Added new carousel feed

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -6,111 +6,146 @@ import { fetchFeedVideos } from "../../redux/actions";
 import SimpleStorage from "react-simple-storage";
 import VideoListItem from "../../components/videoListContainer/videoListItem";
 
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
 import optionIcon from "./options.svg";
 import loadIcon from "./Preloader_5.gif";
 
 import "./feed.scss";
 class Feed extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		let currentFeed = JSON.parse(localStorage.getItem("_currentFeedSelection"));
+    let currentFeed = JSON.parse(localStorage.getItem("_currentFeedSelection"));
 
-		if (currentFeed === undefined || currentFeed === null) {
-			currentFeed = "day";
-		} else {
-			currentFeed = JSON.parse(localStorage.getItem("_currentFeedSelection"));
-		}
+    if (currentFeed === undefined || currentFeed === null) {
+      currentFeed = "day";
+    } else {
+      currentFeed = JSON.parse(localStorage.getItem("_currentFeedSelection"));
+    }
 
-		this.state = {
-			showMenu: false,
-			currentFeedSelection: currentFeed
-		};
-	}
+    this.state = {
+      showMenu: false,
+      currentFeedSelection: currentFeed,
+      responsive: { 300: { items: 5 } }
+    };
+  }
 
-	componentWillMount() {
-		console.log(this.props.clips);
-		this.props.fetchFeedVideos(this.state.currentFeedSelection);
-		if (this.props.clips.length < 1) {
-			console.log(this.props.clips);
-		}
-	}
+  componentWillMount() {
+    console.log(this.props.clips);
+    this.props.fetchFeedVideos(this.state.currentFeedSelection);
+    if (this.props.clips.length < 1) {
+      console.log(this.props.clips);
+    }
+  }
 
-	toggleMenu = () => {
-		this.setState({ showMenu: !this.state.showMenu });
-	};
+  toggleMenu = () => {
+    this.setState({ showMenu: !this.state.showMenu });
+  };
 
-	updateMenu = time => {
-		this.setState({ showMenu: false, currentFeedSelection: time });
+  handleOnDragStart = e => e.preventDefault();
 
-		if (this.props.clips[time] === undefined) {
-			console.log("wut");
-			this.props.fetchFeedVideos(time);
-		}
-	};
+  updateMenu = time => {
+    this.setState({ showMenu: false, currentFeedSelection: time });
 
-	getClips = limit => {
-		const original = this.props.clips[this.state.currentFeedSelection];
-		let clip = original.filter((i, index) => index < limit);
+    if (this.props.clips[time] === undefined) {
+      console.log("wut");
+      this.props.fetchFeedVideos(time);
+    }
+  };
 
-		return clip.map((x, index, arr) => (
-			<Link key={uid(x)} to={{ pathname: `${this.props.match.url}/${x.slug}`, state: { videos: arr, current: index, next: index + 1, prev: index - 1 } }}>
-				<VideoListItem video={x} />
-			</Link>
-		));
-	};
+  getClips = limit => {
+    const original = this.props.clips[this.state.currentFeedSelection];
+    let clip = original.filter((i, index) => index < limit);
 
-	render() {
-		const loadGif = <img src={loadIcon} alt="load icon" />;
-		let clips;
-		if (this.props.clips[this.state.currentFeedSelection] !== undefined) {
-			clips = this.getClips(10);
-		} else {
-			clips = <div>No clips found this {this.state.currentFeedSelection}</div>;
-		}
+    return clip.map((x, index, arr) => (
+      <Link onDragStart={this.handleOnDragStart} key={uid(x)} to={{ pathname: `${this.props.match.url}/${x.slug}`, state: { videos: arr, current: index, next: index + 1, prev: index - 1 } }}>
+        <VideoListItem video={x} />
+      </Link>
+    ));
+  };
 
-		return (
-			<section>
-				<SimpleStorage parent={this} blacklist={["showMenu", "back", "backURL", "name"]} />
-				<div className="top-bar">
-					<div className="videolist-options">
-						<img src={optionIcon} alt="options" />
-						<span>SORT FEED BY</span>
-						<span className="options-choice" onClick={this.toggleMenu}>
-							{this.state.currentFeedSelection}
-						</span>
-						{this.state.showMenu ? (
-							<div className="time-menu">
-								<div onClick={() => this.updateMenu("day")}>Day</div>
-								<div onClick={() => this.updateMenu("week")}>Week</div>
-								<div onClick={() => this.updateMenu("month")}>Month</div>
-								<div onClick={() => this.updateMenu("all")}>All</div>
-							</div>
-						) : null}
-					</div>
-				</div>
+  render() {
+    const loadGif = <img src={loadIcon} alt="load icon" />;
+    let clips;
+    if (this.props.clips[this.state.currentFeedSelection] !== undefined) {
+      clips = this.getClips(20);
+    } else {
+      clips = <div>No clips found this {this.state.currentFeedSelection}</div>;
+    }
 
-				<section className="clips-container">{this.props.loading ? loadGif : clips}</section>
-			</section>
-		);
-	}
+    return (
+      <section>
+        <SimpleStorage parent={this} blacklist={["showMenu", "back", "backURL", "name", "responsive"]} />
+
+        <section className="clips-container-feed">
+          <div className="top-bar-feed">
+            <div className="top-title">TOP TWITCH CLIPS</div>
+            <div className="videolist-options">
+              <img src={optionIcon} alt="options" />
+              <span>SORT FEED BY</span>
+              <span className="options-choice" onClick={this.toggleMenu}>
+                {this.state.currentFeedSelection}
+              </span>
+              {this.state.showMenu ? (
+                <div className="time-menu">
+                  <div onClick={() => this.updateMenu("day")}>Day</div>
+                  <div onClick={() => this.updateMenu("week")}>Week</div>
+                  <div onClick={() => this.updateMenu("month")}>Month</div>
+                  <div onClick={() => this.updateMenu("all")}>All</div>
+                </div>
+              ) : null}
+            </div>
+          </div>
+
+          {this.props.loading ? loadGif : clips}
+        </section>
+
+        <section className="clips-container-feed">
+          <div className="top-bar-feed">
+            <div className="top-title-youtube">TOP YOUTUBE CLIPS</div>
+            <div className="videolist-options">
+              <img src={optionIcon} alt="options" />
+              <span>SORT FEED BY</span>
+              <span className="options-choice" onClick={this.toggleMenu}>
+                {this.state.currentFeedSelection}
+              </span>
+              {this.state.showMenu ? (
+                <div className="time-menu">
+                  <div onClick={() => this.updateMenu("day")}>Day</div>
+                  <div onClick={() => this.updateMenu("week")}>Week</div>
+                  <div onClick={() => this.updateMenu("month")}>Month</div>
+                  <div onClick={() => this.updateMenu("all")}>All</div>
+                </div>
+              ) : null}
+            </div>
+          </div>
+
+          <AliceCarousel items={[1, 2, 3]} responsive={this.state.responsive} mouseDragEnabled>
+            {this.props.loading ? loadGif : clips}
+          </AliceCarousel>
+        </section>
+      </section>
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-	return {
-		clips: state.feedsReducer,
-		loading: state.feedsReducer.loading,
-		error: state.feedsReducer.error
-	};
+  return {
+    clips: state.feedsReducer,
+    loading: state.feedsReducer.loading,
+    error: state.feedsReducer.error
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-	return {
-		fetchFeedVideos: time => dispatch(fetchFeedVideos(time))
-	};
+  return {
+    fetchFeedVideos: time => dispatch(fetchFeedVideos(time))
+  };
 };
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Feed);
